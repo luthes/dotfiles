63fe9d48c0a1420ced0f7ed909728c63400befce
luthes <steve.luther@gmail.com>
2017-02-20 18:22:34 -0800

    Remove old DWM files.

--- lock.sh
+++ lock.sh
@@ -1,5 +1,11 @@
-#!/bin/bash
-#Make Executable, add to /bin/bash
-scrot /tmp/screenshot.png
-convert /tmp/screenshot.png -blur 0x5 /tmp/screenshotblur.png
-i3lock -i /tmp/screenshotblur.png
+#!/usr/bin/env bash
+#Take a Screenshot
+scrot /tmp/screen.png
+#Pixelate the Screenshot
+convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
+#Add Lock image, I think
+convert /tmp/screen.png ~/.dotfiles/lock.png -gravity center -composite -matte /tmp/screen.png
+#Lock the screen with the image.
+i3lock -i /tmp/screen.png
+#Remove temp image.
+rm /tmp/screen.png
